904d58c2b0b1a2965ace31818f73edcc3c083b76
nika9916 <nikoloz.dalakishvili.1@iliauni.edu.ge>
2023-05-20 19:27:25 +0400

    Final commit

---
+++ static/js/app.js
@@ -0,0 +1,114 @@
+const dataAraay = [
+    {
+        userId: 1,
+        userName: 'სახელი 1',
+        userLastName: 'გვარი 1',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 20,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 2,
+        userName: 'სახელი 2',
+        userLastName: 'გვარი 2',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 30,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 3,
+        userName: 'სახელი 3',
+        userLastName: 'გვარი 3',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 35,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 4,
+        userName: 'სახელი 4',
+        userLastName: 'გვარი 4',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 29,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 5,
+        userName: 'სახელი 5',
+        userLastName: 'გვარი 5',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 49,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 6,
+        userName: 'სახელი 6',
+        userLastName: 'გვარი 6',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 31,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 7,
+        userName: 'სახელი 7',
+        userLastName: 'გვარი 7',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 31,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+]
+
+dataAraay.forEach(item => {
+    const number = item.userId;
+    const img = item.userImage;
+    const name = item.userName;
+    const lastName = item.userLastName;
+    const age = item.userAge;
+
+    const address = item.userAddresses[0]
+    // const address = item.userAddresses.forEach(item => {
+    //     item
+    // })
+
+    const card = document.createElement("div");
+    const cardConteiner = document.querySelector(".cards-container");
+    cardConteiner.appendChild(card);
+
+    card.innerHTML = ` 
+        <span>${number}</span>
+        <img src="${img}">
+        <h2>${name}</h2>
+        <h3>${lastName}</h3>
+        <span>${age}</span>
+        <p>${address}</p>
+
+    `
+})
+
